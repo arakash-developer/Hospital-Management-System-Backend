9971143fa50110ee0f6718c07df75535a8014f5e
akash100a <arakash968786@gmail.com>
2025-08-09 18:23:28 +0600

    update

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -6,33 +6,3 @@ router.post("/", login);
 
 
 module.exports = router;
-
-/**
- * @swagger
- * /api/login:
- *   post:
- *     summary: Login
- *     tags: [Login]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - email
- *               - password
- *             properties:
- *               identifier:
- *                 type: string
- *                 format: string
- *                 example: arakash.developer@gmail.com
- *               password:
- *                 type: string
- *                 format: password
- *                 example: akash
- *     responses:
- *       200:
- *         description: Login successful
- */
-
