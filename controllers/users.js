fbbd01487ab95b79706a867d70eac7a67f5c66df
akash100a <arakash968786@gmail.com>
2025-11-15 10:58:11 +0600

    feat: implement user creation logic with validation and error handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,49 @@
-const createUser = (req, res) => {
-  res.send("User created successfully");
-}
+const user = require("../models/user");
+
+const createUser = async (req, res) => {
+  try {
+    const body = req.body || {};
+    const { name, email, username, password, role } = body;
+
+    if (Object.keys(body).length === 0) {
+      return res.status(400).json({ message: "Request body is required" });
+    }
+
+    if (!name || !email || !username || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // Check if email exists
+    const emailExists = await user.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    // Check if username exists
+    const usernameExists = await user.findOne({ username });
+    if (usernameExists) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    const newUser = new user({ name, email, username, password, role });
+    await newUser.save();
+    return res
+      .status(201)
+      .json({ message: "User created successfully", userId: newUser._id });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
 const getUser = (req, res) => {
   res.send("User details");
-}
+};
 const updateUser = (req, res) => {
   res.send("User updated successfully");
-}   
+};
 const deleteUser = (req, res) => {
   res.send("User deleted successfully");
-}
+};
 
 module.exports = {
   createUser,
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  }
+  ,
+    password: {
+    type: String,
+    required: true,
+  },
+  role:{
+    type: String,
+    enum: ['admin', 'doctor', 'nurse', 'patient','receptionist'],
+    default: 'receptionist'
+  }
+}, { timestamps: true });
+
+const users = mongoose.model("User", userSchema);
+module.exports = users;
