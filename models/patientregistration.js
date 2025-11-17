e17b118a210344c4c916c47014f951598903a416
akash100a <arakash968786@gmail.com>
2025-11-17 11:06:27 +0600

    feat: implement patient registration model with procedures and calculation fields

---
+++ models/patientregistration.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const patientRegistrationSchema = new mongoose.Schema({
+  patientid: { type: String, required: true, unique: true },
+  date: { type: Date, required: true },
+  patientname: { type: String, required: true },
+  sex: { type: String, required: true },
+  refDoctor: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
+  age: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  // Procedures array
+  procedures: [
+    {
+      procedureName: { type: String, required: true },
+      procedureCharge: { type: Number, required: true },
+    },
+  ],
+  procedurecalculation: [
+  {
+    name : { type: String, required: true },
+    total : { type: Number, required: true },
+    discount : { type: Number, required: true },
+    discounted : { type: Number, required: true },
+    paid : { type: Number, required: true },
+    due : { type: Number, required: true
+  }
+],
+  totalCharge: { type: Number, default: 0 },
+  totalDiscount: { type: Number, default: 0 },
+  totalDiscounted: { type: Number, default: 0 },
+  totalPaid: { type: Number, default: 0 },
+  totalDue: { type: Number, default: 0 },
+});
+
+const PatientRegistration = mongoose.model(
+  "PatientRegistration",
+  patientRegistrationSchema
+);
+module.exports = PatientRegistration;
